216beab6f050b40f7a650befa42e276e54827c33
kitsune <108506662+foxdevtime@users.noreply.github.com>
2024-03-11 21:47:08 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,19 +3,19 @@
 internal class Program
 {
     using (Mutex mutex = new Mutex(true, "ROBLOX_singletonMutex"))
-{
-    if (!mutex.WaitOne(TimeSpan.Zero, true))
     {
-        Console.WriteLine("Close Roblox!");
-        Thread.Sleep(2000);
-        return;
+        if (!mutex.WaitOne(TimeSpan.Zero, true))
+        {
+            Console.WriteLine("Close Roblox!");
+            Thread.Sleep(2000);
+            return;
+        }
+    
+        Console.WriteLine("Nice cocks!");
+    
+        while (true)
+        {
+            Thread.Sleep(int.MaxValue);
+        }
     }
-
-    Console.WriteLine("Nice cocks!");
-
-    while (true)
-    {
-        Thread.Sleep(int.MaxValue);
-    }
-}
 }
