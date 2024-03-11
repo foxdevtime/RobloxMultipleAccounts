808ddfeca285b9d0d3e3166ee2b0988710a36217
kitsune <108506662+foxdevtime@users.noreply.github.com>
2024-03-11 21:45:59 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,22 +2,20 @@
 
 internal class Program
 {
-    [DllImport("kernel32.dll")]
-    static extern IntPtr GetConsoleWindow();
-
-    [DllImport("user32.dll")]
-    static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
-
-    const int SW_HIDE = 0;
-
-    private static void Main(string[] args)
+    using (Mutex mutex = new Mutex(true, "ROBLOX_singletonMutex"))
+{
+    if (!mutex.WaitOne(TimeSpan.Zero, true))
     {
-        var handle = GetConsoleWindow();
-
-        if (handle != IntPtr.Zero)
-            ShowWindow(handle, SW_HIDE);
-
-        new Mutex(true, "ROBLOX_singletonMutex");
-        Thread.Sleep(Timeout.Infinite);
+        Console.WriteLine("Close Roblox!");
+        Thread.Sleep(2000);
+        return;
     }
+
+    Console.WriteLine("Nice cocks!");
+
+    while (true)
+    {
+        Thread.Sleep(int.MaxValue);
+    }
+}
 }
