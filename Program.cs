8725e3b07938979bc74f99abcabd42917ec86e28
kitsune <108506662+foxdevtime@users.noreply.github.com>
2024-03-11 21:23:23 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+
+internal class Program
+{
+    [DllImport("kernel32.dll")]
+    static extern IntPtr GetConsoleWindow();
+
+    [DllImport("user32.dll")]
+    static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    const int SW_HIDE = 0;
+
+    private static void Main(string[] args)
+    {
+        var handle = GetConsoleWindow();
+
+        if (handle != IntPtr.Zero)
+            ShowWindow(handle, SW_HIDE);
+
+        new Mutex(true, "ROBLOX_singletonMutex");
+        Thread.Sleep(Timeout.Infinite);
+    }
+}
